b7f7602d69e2bfbae05b690bc0437639667c93d1
Neel <send2neelpatel@gmail.com>
2018-01-18 01:07:50 -0500

    claudiaGasm :)

--- bot.js
+++ bot.js
@@ -6,9 +6,12 @@ var fs = require("fs"),
 var totalImages = 1;
 //var p = "../" // current directory
 //var p = "/mnt/c/Users/Neel/Pictures/Screenshots"
-var p = "/Users/Neel/Pictures/Screenshots"
+//var p = "/Users/Neel/Pictures/Screenshots"
+var p = "/Users/Neel/Downloads/claudiaGasm/PadawanClaudia"
+
 
 totalImages = 1;
+var arrPaths = [];
 fs.readdir(p, function(err, files) {
     if (err) {
         throw err;
@@ -19,10 +22,16 @@ fs.readdir(p, function(err, files) {
     }).filter(function(file) {
         return fs.statSync(file).isFile();
     }).forEach(function(file) {
+        arrPaths.push(file);
+
         console.log("%s (%s)", file, path.extname(file));
+        
+        //Store path to file in an array of strings
         totalImages += 1;
     });
 });
+
+
 
 
 // Import the discord.js module
@@ -43,17 +52,18 @@ client.on('ready', () => {
 // Create an event listener for messages
 client.on('message', message => {
     // If the message is "ping"
-    if (message.content === '!ping') {
+    if (message.content === '!claudiaGasm') {
         // Send "pong" to the same channel
 
 
 
 
-        message.channel.send('there are ' + totalImages + ' images');
-
-        var randomVal = getRandomInt(1, totalImages + 1);
+        // message.channel.send('there are ' + totalImages + ' images');
+        console.log("\nCOMMAND HAS BEEN USED!\n");
+       // var randomVal = getRandomInt(1, totalImages + 1);
         var file = new Discord.Attachment();
-        file.setAttachment('/Users/Neel/Pictures/Screenshots/Screenshot (' + randomVal + ').png');
+        var picturePath = arrPaths[Math.floor(Math.random()*arrPaths.length)];
+        file.setAttachment(picturePath);
         message.channel.send(file);
     }
 });
