a39654627a55b2f119c3b3b53fe7d38765497e35
Neel <send2neelpatel@gmail.com>
2018-01-23 21:37:08 -0500

    MANAGED TO GET DATA OUT OF SQL DATABASEEEE

--- bot.js
+++ bot.js
@@ -101,29 +101,56 @@ client.on('message', message => {
 
                 fs.readdirSync(testFolder).forEach(file => {
                 console.log(pathToPicFolder + '/' + file);
+                var filePath = pathToPicFolder + '/' + file;
+                arrPaths.push(filePath);
                 totalImages += 1;
-                })
-
-
-
-
-
-
-
+                });
 
 
                 message.channel.send(totalImages);
 
                 var guildID = message.guild.id;
-                
-                
-                sql.run("CREATE TABLE IF NOT EXISTS commandData (guildID TEXT, isActivated INTEGER, arrOfPaths BLOB, totalPaths INTEGER, directoryPath TEXT)").then(() => {
-                    sql.run (`INSERT INTO commandData (guildID, isActivated, arrOfPaths, totalpaths, directoryPath) VALUES (?,?,?,?,?)`, [guildID,1, arrPaths,totalImages,pathToPicFolder]);
-                  });
+               
 
+                /*
+                SQL TABLE 
+                ============
+                guildID  |  isCLGActivated | clgTotalImages | pathToPicFolder 
+                */
+                /*
+                sql.run(`UPDATE commandData SET isCLGActivated = ${1}, clgTotalImages = ${totalImages} , clgPathToPicFolder = ${pathToPicFolder} WHERE guildID = ${guildID}`).then (row => {
+                    if (!row){
+                        sql.run("INSERT INTO commandData (guildID, isCLGActivated, clgTotalImages, clgPathToPicFolder) VALUES (?, ?, ?, ?)", [guildID, 1, totalImages, pathToPicFolder]);
+                    }
+                    else
+                    {
+                        sql.run(`UPDATE commandData SET isCLGActivated = ${1}, clgTotalImages = ${totalImages} , clgPathToPicFolder = ${pathToPicFolder} WHERE guildID = ${guildID}`);
+                    }
+
+                }).catch(() => {
+                    sql.run("CREATE TABLE IF NOT EXISTS commandData (guildID TEXT, isCLGActivated INTEGER, clgTotalImages INTEGER, clgPathToPicFolder TEXT)").then(() => {
+                        sql.run("INSERT INTO commandData (guildID, isCLGActivated, clgTotalImages, clgPathToPicFolder) VALUES (?, ?, ?, ?)", [guildID, 1, totalImages, pathToPicFolder]);
+                    })
+                });*/
+                
+                sql.run(`UPDATE commandData SET isCLGActivated = ${1}, clgTotalImages = ${totalImages} , clgPathToPicFolder = ${pathToPicFolder}, clgArrOfPaths = ${arrPaths} WHERE guildID = ${guildID}`).then (row => {
+                    if (!row){
+                        sql.run("INSERT INTO commandData (guildID, isCLGActivated, clgTotalImages, clgPathToPicFolder, clgArrOfPaths) VALUES (?, ?, ?, ?, ?)", [guildID, 1, totalImages, pathToPicFolder, arrPaths]);
+                    }
+                    else
+                    {
+                        sql.run(`UPDATE commandData SET isCLGActivated = ${1}, clgTotalImages = ${totalImages} , clgPathToPicFolder = ${pathToPicFolder}, clgArrOfPaths = ${arrPaths} WHERE guildID = ${guildID}`);
+                    }
+
+                }).catch(() => {
+                    sql.run("CREATE TABLE IF NOT EXISTS commandData (guildID TEXT, isCLGActivated INTEGER, clgTotalImages INTEGER, clgPathToPicFolder TEXT, clgArrOfPaths BLOB)").then(() => {
+                        sql.run("INSERT INTO commandData (guildID, isCLGActivated, clgTotalImages, clgPathToPicFolder, clgArrOfPaths) VALUES (?, ?, ?, ?, ?)", [guildID, 1, totalImages, pathToPicFolder, arrPaths]);
+                    })
+                });
 
                 
-                message.channel.send(`ClaudiaGasm activated ${guildID} stuff ${pathToPicFolder}`);
+                message.channel.send(`ClaudiaGasm activated`);
+                return;
             }
             return;
         } // end activateClaudiaGasm
@@ -141,121 +168,20 @@ client.on('message', message => {
             */
            // message.channel.send("running claudiaGasm");
             
-            var getItemResolved = false;
-            var pathToPicFolder;
-            var arrPaths = [];
-            var totalImages = 1;
+//            var getItemResolved = false;
 
 
             message.channel.send("Starting  getItem");
-            while (!getItemResolved){
-                sql.get(`SELECT * FROM commandData WHERE guildID = "${message.guild.id}"`).then(row => {
-                    if (!row){
-                        return message.reply("ERROR 1");
-                    }
-                    pathToPicFolder = row.directoryPath;
-                    getItemResolved = true;
-                });
-            }
-            message.channel.send("ending getItem");
-        /*  
-            if (!fs.existsSync(pathToPicFolder)) {
-                message.channel.send('Error 404: Sorry. Please try again, and contact @Neel#2970 and he will fix me as soon as possible. Thank you.');
-                return;
-            }
-            message.channel.send(pathToPicFolder);
-
-
-            message.channel.send("Starting  Looping");
-            let directoryLooping = false;
-
-            while(!directoryLooping){
-                fs.readdir(pathToPicFolder, function(err,files){
-                    if (err) {
-                        throw err;
-                    }
-                
-                    files.map(function(file){
-                        return path.join(pathToPicFolder, file);
-                    }).filter(function (file){
-                        return fs.statSync(file).isFile();
-                    }).forEach(function(file){
-                        arrPaths.push(file);
-                        totalImages += 1;
-                        console.log("%s", file, path.extname(file));
-                    })    
-                })
-
-                directoryLooping = true;
-            }
-            message.channel.send("Looping end");
-            
-
+            sql.get(`SELECT * FROM commandData WHERE guildID ="${message.guild.id}"`).then(row => {
+                if (!row) return message.reply("ERROR: Your current level is 0");
+                message.reply(`Your current level is ${row.clgTotalImages}`);
+                var rcvRowPaths = 
+                message.reply(`ArrayOfPaths = ${row.clgArrOfPaths}`)
+              });
+             
+            // close the database connection
+            //db.close();
             /*
-            sql.get(`SELECT * FROM commandData WHERE guildID = "${message.guild.id}"`).then(row =>{
-                if (!row){
-                    return message.reply("ERROR 1");
-                }
-               // message.reply(`FIELD IS ${row.isActivated}  -  ${row.directoryPath}`);
-                pathToPicFolder = row.directoryPath;
-                if (!fs.existsSync(pathToPicFolder)) {
-                    message.channel.send('Error 404: Sorry. Please try again, and contact @Neel#2970 and he will fix me as soon as possible. Thank you.');
-                    return;
-                }
-
-                message.channel.send( pathToPicFolder);
-                fs.readdir(pathToPicFolder, function(err,files){
-                    if (err) {
-                        throw err;
-                    }
-                
-                    files.map(function(file){
-                        return path.join(pathToPicFolder, file);
-                    }).filter(function (file){
-                        return fs.statSync(file).isFile();
-                    }).forEach(function(file){
-                        arrPaths.push(file);
-                        totalImages += 1;
-                        console.log("%s", file, path.extname(file));
-                    })    
-                })
-
-                console.log ("\nINSIDE Found %d pictures. %d\n", totalImages, arrPaths.length);
-                message.channel.send("claudiaGasm end");
-            });
-
-            console.log ("\nOUTSIDE Found %d pictures. %d\n", totalImages, arrPaths.length);    */         
-            //message.channel.send( pathToPicFolder);
-                
-
-            //totalImages = 1;
-            //arrPaths = [];
-         
-               
-            /*
-            fs.readdir(pathToPicFolder, function(err,files){
-                if (err) {
-                    throw err;
-                }
-            
-                files.map(function(file){
-                    return path.join(pathToPicFolder, file);
-                }).filter(function (file){
-                    return fs.statSync(file).isFile();
-                }).forEach(function(file){
-                    arrPaths.push(file);
-                    totalImages += 1;
-                   // console.log("%s (%s)", file, path.extname(file));
-                })    
-            })
-
-
-
-            console.log ("Found %d pictures. %d", totalImages, arrPaths.length);
-
-
-
-
             var file = new Discord.Attachment();
             var picturePath = arrPaths[Math.floor(Math.random()*arrPaths.length)];
             try {
@@ -267,7 +193,9 @@ client.on('message', message => {
     
             file.setAttachment(picturePath);
             message.channel.send(file);
-*/  
+            */
+
+
 
             message.channel.send("claudiaGasm end");
 
@@ -284,37 +212,7 @@ client.on('message', message => {
 
 }) // client on
 
-
-/*
-// Create an event listener for messages
-    client.on('message', message => {
-    // If the message is "ping"
-    if (message.content === '!claudiaGasm1') {
-        // Send "pong" to the same channel
-
-        if (isRefreshed != 1){
-            message.channel.send('Folder has not been refreshed. Cannot continue.');
-            return;
-        }
-
-
-        // message.channel.send('there are ' + totalImages + ' images');
-        console.log("\nCOMMAND HAS BEEN USED %dX!\n", commandCounter);
-        commandCounter++;
-       // var randomVal = getRandomInt(1, totalImages + 1);
-        var file = new Discord.Attachment();
-        var picturePath = arrPaths[Math.floor(Math.random()*arrPaths.length)];
-        try {
-           fs.accessSync(picturePath);
-        } catch (e) {
-          fs.mkdirSync(picturePath);
-          message.channel.send('Error 404: Sorry. Please try again, and contact @Neel#2970 and he will fix me as soon as possible. Thank you.')
-        }
-
-        file.setAttachment(picturePath);
-        message.channel.send(file);
-    }
-    else if (message.content === "!refreshFolder"){
+    /*if (message.content === "!refreshFolder"){
         
         totalImages = 1;
         arrPaths = [];
