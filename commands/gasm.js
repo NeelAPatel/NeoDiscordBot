f65567ecbff9bce4c97a58a7cc930df7782069de
Neel <send2neelpatel@gmail.com>
2018-09-16 03:19:16 -0400

    Changed the if-statement menu selection to switch case
    Added help data as well

--- commands/gasm.js
+++ commands/gasm.js
@@ -67,61 +67,73 @@ exports.run = async(client, message, args, level) => {
 				}
 				
 			}   
-            else if (args[0] === "setpath" || args[0] === "trace") //!gasm setpath <path>
-            {
-				//Gets the Path argument
-				userPath = args[1];
+			else{
+				switch (args[0]){
+					case "setpath":
+					case "tracefolder":	{
+						//Gets the Path argument
+						userPath = args[1];
 
-				//Checks if the path is valid
-				if (userPath === undefined)
-					msgFormat.err(message, "ERROR!","Missing path argument");
-				else if (!fs.existsSync(userPath)) 
-					msgFormat.err(message, "ERROR!","Path does not exist.");
-				else{
-					//Path is valid!
-					
-					//Stores the path in gasmDB
-					client.gasmDB.setProp(guildKey, "folderPath", args[1]);
-					
-					//Gets the array of paths from gasmDB
-					let arrPaths = client.gasmDB.getProp(guildKey, "arrPaths");
-					arrPaths = []; // resets the array to empty
-					fs.readdir(userPath, function(err,files) { //readdir is asynchronous
-						if (err) { throw err; }
-						/* Fetches each from the given userPath 
-						and adds its path to the array */
-						files.map(function(file){
-							return path.join(userPath, file);
-						}).filter(function (file){
-							return fs.statSync(file).isFile();
-						}).forEach(function(file){
-							arrPaths.push(file);
-							console.log("%s (%s)", file, path.extname(file));
-						})    
+						//Checks if the path is valid
+						if (userPath === undefined)
+							msgFormat.err(message, "ERROR!","Missing path argument");
+						else if (!fs.existsSync(userPath)) 
+							msgFormat.err(message, "ERROR!","Path does not exist.");
+						else{
+							//Path is valid!
+							
+							//Stores the path in gasmDB
+							client.gasmDB.setProp(guildKey, "folderPath", args[1]);
+							
+							//Gets the array of paths from gasmDB
+							let arrPaths = client.gasmDB.getProp(guildKey, "arrPaths");
+							arrPaths = []; // resets the array to empty
+							fs.readdir(userPath, function(err,files) { //readdir is asynchronous
+								if (err) { throw err; }
+								/* Fetches each from the given userPath 
+								and adds its path to the array */
+								files.map(function(file){
+									return path.join(userPath, file);
+								}).filter(function (file){
+									return fs.statSync(file).isFile();
+								}).forEach(function(file){
+									arrPaths.push(file);
+									console.log("%s (%s)", file, path.extname(file));
+								})    
+								
+								// adds new array to gasmDB
+								client.gasmDB.setProp(guildKey,"arrPaths", arrPaths);
+							});
+
+							//Wrapping up: Let user know the operation is complete + the path that was traced
+							
+							let folderPath = client.gasmDB.getProp(guildKey, "folderPath");
+							msgFormat.status(message,"Success!","Folder trace complete!\n Traced Path: " + `${folderPath}`);
+						}
 						
-						// adds new array to gasmDB
-						client.gasmDB.setProp(guildKey,"arrPaths", arrPaths);
-					});
-
-					//Wrapping up: Let user know the operation is complete + the path that was traced
+						break;
+					}
+					case "getpath":{
+						// return the currently traced path
+						let folderPath = client.gasmDB.getProp(guildKey, "folderPath");
+						msgFormat.status(message,"Current Path:",`${folderPath}`);
 					
-					let folderPath = client.gasmDB.getProp(guildKey, "folderPath");
-					msgFormat.status(message,"Success!","Folder trace complete!\n Traced Path: " + `${folderPath}`);
-				}
-			}
-			else if (args[0] === "getpath"){
-				// return the currently traced path
-				let folderPath = client.gasmDB.getProp(guildKey, "folderPath");
-				msgFormat.status(message,"Current Path:",`${folderPath}`);
-			}
-			else if (args[0] === "delFromDB"){
-				// removes date from gasmDB
-				if (client.gasmDB.has(guildKey)){
-					client.gasmDB.delete(guildKey);
-					msgFormat.status(message,"Success!", "!gasm data has for this guild has been deleted");
-				}
-				else{
-					msgFormat.err(message,"Not Found", "!gasm data has for this guild could not be found.");
+						break;
+					}
+					case "delFromDB":{
+						// removes date from gasmDB
+						if (client.gasmDB.has(guildKey)){
+							client.gasmDB.delete(guildKey);
+							msgFormat.status(message,"Success!", "!gasm data has for this guild has been deleted");
+						}
+						else{
+							msgFormat.err(message,"Not Found", "!gasm data has for this guild could not be found.");
+						}
+						break;
+					}
+					case "help":{
+						showHelp(message);
+					}
 				}
 			}
 		}
@@ -153,6 +165,38 @@ function addDefaultDB(client,guildKey){
 	});
 }
 
+
+function showHelp(message){
+    message.channel.send("!gasm allows a server to upload a random picture from a folder on the host's computer. If something goes wrong, contact the Bot Admin: `Neel#2970` \n__**The following sub commands are available for use:** __")
+    const embed = {
+        "title": "!gasm help",
+        "color": 16098851,
+        "fields": [
+          {
+            "name": "!gasm",
+            "value": "Uploads a random picture from a designated folder."
+          },
+          {
+            "name": "setpath <path> || tracefolder <path>",
+            "value": "Sets the new path and analyzes the folder for future use"
+          },
+          {
+            "name": "getpath",
+            "value": "Returns the current path"
+          },
+          {
+            "name": "delFromDB",
+            "value": "WARNING: This deletes the server's entry from the !gasm database, you will need to set it up again."
+          },
+          {
+            "name": "help",
+            "value": "Shows this message again"
+          }
+        ]
+      };
+
+    message.channel.send({embed});
+}
 
 exports.conf = {
     enabled: true,
